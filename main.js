a46cc871704f1dfd4fc1ef86eb238f10bdb55cea
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-08-16 12:40:27 +0530

    api sucsess

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-var inputval = document.querySelector('.cityinput')
-var button = document.querySelector('.submit')
-var city = document.querySelector('.cityoutput')
-var temp = document.querySelector('.temp')
+var inputval = document.querySelector('#cityinput')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var temp = document.querySelector('#temp')
 
 apik = '3045dd712ffe6e702e3245525ac7fa38'
 
 
-// window.onload=function(){
-document.addEventListener('DOMContentLoaded', function () {
-button.addEventListener("click", ()=>{
-    fetch(`api.openweathermap.org/data/2.5/weather?q=${inputval}&appid=${apik}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => alert('Wrong city name'))
-})
-})
+ window.onload=function(){
+    btn.addEventListener('click', function(){
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
+        .then(res => res.json())
+        .then(data => {
+            var nameval = data['name']
+            var tempature = data['main']['temp']
+            var windspeed = data['weather']['0']['description']
+            var humidity = data['main']['humidity']
+
+
+            
+
+        })
+        .catch(err => alert('Wrong city name'))
+    })
+}
