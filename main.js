f47e100ffd49c2a78cfe1c4957ef5b523324e3a5
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-10-06 19:37:46 +0530

    fetch done

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var inputval = document.querySelector('#cityinput')
 var btn = document.querySelector('#add');
 var city = document.querySelector('#cityoutput')
 var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+var humid = document.querySelector('#humidity')
+var descrip = document.querySelector('#description')
 
 apik = '3045dd712ffe6e702e3245525ac7fa38'
 
@@ -13,11 +16,15 @@ apik = '3045dd712ffe6e702e3245525ac7fa38'
         .then(data => {
             var nameval = data['name']
             var tempature = data['main']['temp']
-            var windspeed = data['weather']['0']['description']
+            var descrip = data['weather']['0']['description']
             var humidity = data['main']['humidity']
+            // var descValue = data['weather'][0]['description']
 
+            city.innerHTML=nameval
+            temp.innerHTML = 'Temparature - '+ tempature
+            description.innerHTML = descrip
+            humid.innerHTML = 'Humidity - ' + humidity
 
-            
 
         })
         .catch(err => alert('Wrong city name'))
