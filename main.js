00b0a94358e6e470651fa0d458d9dbc017f1522e
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-10-22 20:43:43 +0530

    temparature convertion kelvin to celcious

--- main.js
+++ main.js
@@ -9,12 +9,15 @@ var descrip = document.querySelector('#description')
 
 apik = '3045dd712ffe6e702e3245525ac7fa38'
 
+function convertion(val){
+    return (val - 273)
+}
 
  //window.onload=function(){
     btn.addEventListener('click', function(){
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
-         .then(data => console.log(data))
+         //.then(data => console.log(data))
         .then(data => {
             var nameval = data['name']
             var tempature = data['main']['temp']
@@ -23,7 +26,7 @@ apik = '3045dd712ffe6e702e3245525ac7fa38'
             var wndspd = data['wind']['speed']
 
             city.innerHTML=`City: ${nameval}`
-            temp.innerHTML = `Temparature: ${ tempature}`
+            temp.innerHTML = `Temparature: ${ convertion(tempature)}`
             description.innerHTML = `Conditions: ${descrip}`
             humid.innerHTML = `Humidity: ${ humidity}`
             wind.innerHTML = `Wind Speed: ${wndspd}`
