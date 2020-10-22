1d23a0cd5f448b0587fd05b5b27c0951fe3ad367
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-10-22 20:41:07 +0530

    js change 2

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
+//selector variable
 var inputval = document.querySelector('#cityinput')
 var btn = document.querySelector('#add');
 var city = document.querySelector('#cityoutput')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
-var humid = document.querySelector('#humidity')
+var humid = document.querySelector('#humidi')
 var descrip = document.querySelector('#description')
 
 apik = '3045dd712ffe6e702e3245525ac7fa38'
 
 
- window.onload=function(){
+ //window.onload=function(){
     btn.addEventListener('click', function(){
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
+         .then(data => console.log(data))
         .then(data => {
             var nameval = data['name']
             var tempature = data['main']['temp']
             var descrip = data['weather']['0']['description']
             var humidity = data['main']['humidity']
-            // var descValue = data['weather'][0]['description']
+            var wndspd = data['wind']['speed']
 
             city.innerHTML=`City: ${nameval}`
             temp.innerHTML = `Temparature: ${ tempature}`
             description.innerHTML = `Conditions: ${descrip}`
             humid.innerHTML = `Humidity: ${ humidity}`
-
+            wind.innerHTML = `Wind Speed: ${wndspd}`
 
         })
         .catch(err => alert('Wrong city name'))
     })
-}
+//}
