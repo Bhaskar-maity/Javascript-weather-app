6ad1f3c2120386a3397c146ca5501378b4cb0414
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-10-22 23:44:22 +0530

    update main

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 //selector variable
 var inputval = document.querySelector('#cityinput')
 var btn = document.querySelector('#add');
@@ -6,10 +7,10 @@ var descrip = document.querySelector('#description')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
 
-//hide api
-require('dotenv').config()
 
-apik = process.env.API_KEY;
+// Get your own free OWM API key at https://www.openweathermap.org/appid - please do not re-use mine!
+// You don't need an API key for this to work at the moment, but this will change eventually.
+apik = "3045dd712ffe6e702e3245525ac7fa38"
 //kelvin to celcious
 function convertion(val){
     return (val - 273).toFixed(2)
