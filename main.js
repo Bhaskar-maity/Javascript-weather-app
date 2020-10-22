8115fe527a6d9732b44b8b51414fcc7cfe11e43e
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-10-22 20:54:30 +0530

    fetching problem solved

--- main.js
+++ main.js
@@ -2,36 +2,33 @@
 var inputval = document.querySelector('#cityinput')
 var btn = document.querySelector('#add');
 var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
-var humid = document.querySelector('#humidi')
-var descrip = document.querySelector('#description')
+
 
 apik = '3045dd712ffe6e702e3245525ac7fa38'
 
 function convertion(val){
-    return (val - 273)
+    return (val - 273).toFixed(2)
 }
-
- //window.onload=function(){
+//fetch
     btn.addEventListener('click', function(){
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
          //.then(data => console.log(data))
         .then(data => {
             var nameval = data['name']
-            var tempature = data['main']['temp']
             var descrip = data['weather']['0']['description']
-            var humidity = data['main']['humidity']
+            var tempature = data['main']['temp']
             var wndspd = data['wind']['speed']
 
             city.innerHTML=`City: ${nameval}`
-            temp.innerHTML = `Temparature: ${ convertion(tempature)}`
+            temp.innerHTML = `Temparature: ${ convertion(tempature)} C`
             description.innerHTML = `Conditions: ${descrip}`
-            humid.innerHTML = `Humidity: ${ humidity}`
-            wind.innerHTML = `Wind Speed: ${wndspd}`
+            wind.innerHTML = `Wind Speed: ${wndspd} km/h`
 
         })
         .catch(err => alert('Wrong city name'))
     })
-//}
+
